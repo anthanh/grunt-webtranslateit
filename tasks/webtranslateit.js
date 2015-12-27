53c909f1e001efc43cfb4fc5689146d118795375
Anton Pawlik <anton.pawlik@gmail.com>
2015-12-28 00:39:55 +0100

    now able to download multiple files

--- tasks/webtranslateit.js
+++ tasks/webtranslateit.js
@@ -10,90 +10,66 @@
 
 module.exports = function(grunt) {
 
-    // Please see the Grunt documentation for more information regarding task
-    // creation: http://gruntjs.com/creating-tasks
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
 
-    grunt.registerMultiTask('webtranslateit', 'Automatically retrieve webtranslateit.com files to your project', function() {
+  grunt.registerMultiTask('webtranslateit', 'Automatically retrieve webtranslateit.com files to your project', function() {
+    var request = require('request'),
+    async = require('async'),
+    options = this.options({
+      projectToken: 'PUBLIC_KEY',
+      langs: [],
+      dest: this.data.dest || 'tmp'
+    }),
+    urlBase = 'https://webtranslateit.com/api/projects/' + options.projectToken;
 
-        var request = require('request'),
-            async = require('async'),
+    if (typeof options.langs === 'string') {
+      options.langs = [options.langs];
+    }
 
-            options = this.options({
-                projectToken: 'PUBLIC_KEY',
-                langs: [],
-                dest: this.data.dest || 'tmp'
-            }),
+    if (options.dest.substr(-1) !== '/') {
+      options.dest += '/';
+    }
 
-            urlBase = 'https://webtranslateit.com/api/projects/' + options.projectToken;
-
-        if (typeof options.langs === 'string') {
-            options.langs = [options.langs];
+    function downloadLocales(cb) {
+      request(urlBase + '.json', function(error, response) {
+        if (error) {
+          return cb(error);
+        }
+        response.body = JSON.parse(response.body);
+        if (response.statusCode !== 200) {
+          return cb(response.body.error);
         }
 
-        if (options.dest.substr(-1) !== '/') {
-            options.dest += '/';
-        }
+        async.each(response.body.project['project_files'], function(file, cb2){
+          var fileName = file.name.replace('./locales/', '');
+          var filePath = options.dest + fileName;
+          var fileLang = file.locale_code;
+          var localeUrl = urlBase + '/files/' + file.id + '/locales/' + fileLang;
 
-        function getMasterProjectFileId(cb) {
-            request(urlBase + '.json', function(error, response) {
-                if (error) {
-                    return cb(error);
-                }
-                response.body = JSON.parse(response.body);
-                if (response.statusCode !== 200) {
-                    return cb(response.body.error);
-                }
-                var masterProjectFileId = response.body.project['project_files'][0];
-                masterProjectFileId = masterProjectFileId['master_project_file_id'] || masterProjectFileId.id;
+          if (options.langs.indexOf(fileLang) === -1) {
+            return cb();
+          }
 
-                cb(error, masterProjectFileId);
-            });
-        }
-
-        function dowwloadLocales(masterProjectFileId, cb) {
-            if (!masterProjectFileId) {
-                return cb('error undefined masterprojectfileid');
-            }
-
-            var localeUrl = urlBase + '/files/' + masterProjectFileId + '/locales/';
-
-            var localesUrl = options.langs.map(function(langName) {
-                return localeUrl + langName;
-            });
-
-            async.map(localesUrl, request, function(err, responses) {
-
-                if (err) {
-                    return cb(err, responses);
-                }
-
-                responses.map(function(response, index) {
-                    response.body = JSON.parse(response.body);
-
-                    if (response.statusCode !== 200) {
-                        return cb(response.body.error + ' "' + options.langs[index] + '"');
-                    }
-
-                    var file = options.dest + options.langs[index] + '.json',
-                        data = JSON.stringify(response.body, null, '  ');
-                    grunt.file.write(file, data);
-                    grunt.log.writeln('File "' + options.dest + options.langs[index] + '.json' + '" created.');
-                });
-
-                cb();
-            });
-
-        }
-
-        var done = this.async();
-        async.waterfall([getMasterProjectFileId, dowwloadLocales], function(err) {
+          request(localeUrl, function(err, fileResponse) {
             if (err) {
-                grunt.log.errorlns('Got an error: ', err);
-                grunt.fail.fatal(err);
+              return cb2(err, fileResponse);
             }
-            done();
-        });
 
-    });
+            if (response.statusCode !== 200) {
+              fileResponse.body = JSON.parse(fileResponse.body);
+              return cb(fileResponse.body.error + ' "' + fileName + '"');
+            }
+
+            grunt.file.write(filePath, fileResponse.body);
+            grunt.log.ok('File ' + filePath.cyan + ' created.');
+            cb2();
+          });
+        }, cb);
+      });
+    }
+
+    downloadLocales(this.async());
+  });
 
 };
